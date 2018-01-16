0f4432d16963a6c3417c87238d79dc8dd8353b08
FeeVeX <matikile@hotmail.com>
2018-01-16 13:27:31 +0000

    Exp per monster

--- src/Test.java
+++ src/Test.java
@@ -4,18 +4,20 @@
 public class Test {
     static void test(){
         Player player = new Player(1,0,10,2,30);
-        Monster mon = new Monster(100,30,2);
+        Monster mon = new Monster(100,30,1);
 
-        player.addEx(100);
-        player.addEx(100);
-        player.addEx(100);
-        player.addEx(100);
+        //System.out.println(player.experience);
+
+        player.addEx(Math.round((Math.pow((mon.level * ((mon.level + 1) - (player.level * 0.5))),2)/Math.pow((player.level * 0.2),1.6))));
+
+        System.out.println(player.level);
+
+       // System.out.println((Math.round((Math.pow((mon.level * 1.2),2)/Math.pow((player.level / 1.2),0.5)))));
 
        // System.out.println(121.1);
         //System.out.println((int)Math.floor((121.1)));
 
    //     player.attack(mon);
 
-        System.out.println(player.level);
     }
 }
